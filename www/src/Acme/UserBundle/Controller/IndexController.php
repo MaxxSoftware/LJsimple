4d879b70c99f8d03e94a47a6c0d60aafac601204
Sergey <idnarps@gmail.com>
2012-08-26 17:44:02 +0400

    Профиль пользователя: как-то так

--- www/src/Acme/UserBundle/Controller/IndexController.php
+++ www/src/Acme/UserBundle/Controller/IndexController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Acme\IndexBundle\Entity\Users;
+use Acme\IndexBundle\Entity\Posts;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -14,19 +15,50 @@ class IndexController extends Controller
 {
 
   /**
-  * @Route("/{name}/", name="profile")
+  * @Route("/{login}/", name="profile")
   * @Template()
   */
-  public function indexAction($name) {
+  public function indexAction($login) {
     $user = $this->getDoctrine()
             ->getRepository('AcmeIndexBundle:Users')
-            ->findByName($name);
-    return $this->render('AcmeUserBundle:Index:index.html.twig', array(
-      'name' => $user->getName(),
-      'login' => $user->getLogin(),
-      'email' => $user->getEmail()
-    ));
+            ->findOneByLogin($login);
+    if ($user) {
+      $lastPost = $this->findLastPost($user->getId());
+      return $this->render('AcmeUserBundle:Index:index.html.twig', array(
+        'name' => $user->getName(),
+        'login' => $user->getLogin(),
+        'email' => $user->getEmail(),
+        'num_post' => $this->findNumberUserPosts($user->getId()),
+        'last_post' => $lastPost[0]->getTitle(),
+      ));
+    } else {
+      return $this->render('AcmeUserBundle:Index:nouser.html.twig');
+    }
   }
-    
+
+  protected function findNumberUserPosts($id){
+    $query = $this->getDoctrine()->getEntityManager()
+                  ->createQuery('SELECT COUNT(p.id) FROM AcmeIndexBundle:Posts p WHERE p.user=:id')->setParameter('id', $id);
+    try {
+        $num = $query->getSingleResult();
+        return $num[1];
+    } catch (\Doctrine\ORM\NoResultException $e) {
+        return null;
+    }
+  }
+
+  protected function findLastPost($id){
+    $repository = $this->getDoctrine()
+        ->getRepository('AcmeIndexBundle:Posts');
+    $query = $repository->createQueryBuilder('p')
+        ->where('p.user = :id')
+        ->setParameter('id', $id)
+        ->orderBy('p.createDate', 'DESC')
+        ->setFirstResult( 0 )
+        ->setMaxResults( 1 )
+        ->getQuery();
+
+    return $post = $query->getResult();
+  }
 
 }
---
+++ www/src/Acme/UserBundle/Repository/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Методы со сложными запросами для сущности User
+ * User: IdnarpS
+ * Date: 26.08.12
+ */
+namespace Acme\IndexBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository
+{
+
+}
